51dbee50e89518ee0d1bbccf6d9f3ef9b9995ed5
anouar <anouar.bakouch@usmba.ac.ma>
2023-04-19 23:56:35 +0000

    adding titles to layout

--- routes/base.js
+++ routes/base.js
@@ -6,6 +6,26 @@ router.get("/", (_, res, __) => {
     res.render("index", {title: "Main page", ha: "Loco"})
 });
 
+router.get("/partenariats", (_, res, __) => {
+    res.render("partenariats", {title: "Partenariats"})
+});
+
+router.get("/contact", (_, res, __) => {
+    res.render("contact", {title: "Contact"})
+});
+
+router.get("/activites", (_, res, __) => {
+    res.render("activites", {title: "Activités"})
+});
+
+router.get("/membres", (_, res, __) => {
+    res.render("membres", {title: "Membres"})
+});
+
+router.get("/evenements", (_, res, __) => {
+    res.render("evenements", {title: "Événements"})
+});
+
 
 
 export default router;
