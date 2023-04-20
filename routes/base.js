6383f2ed61c9b56970df3044337522ed97af8a1b
anouar <anouar.bakouch@usmba.ac.ma>
2023-04-20 07:17:37 +0000

    initial thinking of implementation

--- routes/base.js
+++ routes/base.js
@@ -3,7 +3,15 @@ import {Router} from "express";
 const router = Router();
 
 router.get("/", (_, res, __) => {
-    res.render("index", {title: "Main page", ha: "Loco"})
+
+    // this array is just temporary | my idea was to pass events as param
+    const events = [
+        { title: "Event 1", imgurl: "image1.jpg" },
+        { title: "Event 2", imgurl: "image2.jpg" },
+        { title: "Event 3", imgurl: "image3.jpg" }
+      ];
+    
+      res.render('index', { title: 'Main page', events: events });
 });
 
 router.get("/partenariats", (_, res, __) => {
@@ -14,16 +22,8 @@ router.get("/contact", (_, res, __) => {
     res.render("contact", {title: "Contact"})
 });
 
-router.get("/activites", (_, res, __) => {
-    res.render("activites", {title: "Activités"})
-});
-
 router.get("/membres", (_, res, __) => {
     res.render("membres", {title: "Membres"})
-});
-
-router.get("/evenements", (_, res, __) => {
-    res.render("evenements", {title: "Événements"})
 });
 
 
@@ -34,6 +34,8 @@ router.get("/ProductionScientifique/reverseEngineering", (_, res, __) => {
 router.get("/ProductionScientifique/communicationScientifique", (_, res, __) => {
     res.render("communicationScientifique", {title: "Communication Scientifique"})
 });
+
+
 
 
 export default router;
