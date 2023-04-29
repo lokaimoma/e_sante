e18746be4a01e1a535e4a2ea91e761a86c30b183
anouar <anouar.bakouch@usmba.ac.ma>
2023-04-29 02:29:00 +0100

    dealing with carousel

--- index.js
+++ index.js
@@ -7,9 +7,12 @@ const __dirname = path.dirname(".");
 const app = express();
 const isProd = process.env.NODE_ENV;
 
+
 isProd !== "production" && app.use("/static", express.static("static"));
 app.set("views", path.join(__dirname, "templates"));
 app.set("view engine", "pug");
 app.use("/", BaseRouter);
+
+
 
 app.listen(8080, () => console.log("\u001b[32m[INFO]\u001b[0m App started successfully on http://localhost:8080"));
