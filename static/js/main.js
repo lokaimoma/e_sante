8c96688abdca68a03d7a5164e7009e1754b2cbf8
Owusu Kelvin Clark <kelvinclark2k77@gmail.com>
2023-05-22 13:49:31 +0100

    feat: Add mobile nav

--- static/js/main.js
+++ static/js/main.js
@@ -1,1 +1,24 @@
+"use strict";
+
+const mobNav = document.getElementById("mob-nav");
+const navChlds = Array.from(mobNav.children)
+let navOpen = false;
+
+function toggleMobNav() {
+  if (!navOpen) {
+    if (mobNav) {
+      mobNav.style.width = '100%';
+      navChlds.forEach(chld => chld.classList.remove("hidden"));
+      navOpen = true;
+      document.body.style.overflow = "hidden";
+    }
+  } else {
+    if (mobNav) {
+      mobNav.style.width = '0%';
+      navOpen = false;
+      navChlds.forEach(chld => chld.classList.add("hidden"));
+      document.body.style.overflow = "";
+    }
+  }
+}
 
